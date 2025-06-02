f60d5615a677c200d157b045fc0c6f7c647c6b3d
xueqiancheng8 <xueqian.cheng@emory.edu>
2025-06-02 13:55:03 +0100

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,6 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
-  // site: { title: "講評世界" },
+  // site: { title: "圆羊庄" },
   // seo: { twitter: "@moeyua13" },
 }
